e8bdcb4b189ec34fe8955c06b9288f1557b57e79
UserOfNames <lightninguy.yoyo@gmail.com>
2023-03-30 17:01:13 -0400

    Restructured directories and prepared flake.nix for expansion

--- flake.nix
+++ flake.nix
@@ -1,30 +1,40 @@
 {
-  description = "NixOS configuration with home manager (minimal)";
+  description = "NixOS configuration with home manager";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = {
+  outputs = inputs@{
+    self,
     home-manager,
     nixpkgs,
     ...
   }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+	config = { allowUnfree = true; };
+      };
+      lib = nixpkgs.lib;
 
-  {
-    nixosConfigurations = {
-      nyx = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.zdbg = import ./home.nix;
-          }
-        ];
+    in {
+      nixosConfigurations = {
+
+        # Main desktop
+        nyx = lib.nixosSystem {
+          inherit system;
+	  modules = [
+	    ./hosts/nyx/nyx.nix 
+            home-manager.nixosModules.home-manager
+	    {
+	      home-manager.useGlobalPkgs = true;
+	      home-manager.useUserPackages = true;
+	      home-manager.users.zdbg = import ./home.nix;
+	    }
+	  ];
+        };
       };
     };
-  };
 }
