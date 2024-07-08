591e0e7a893aecc0b2f8854d3bb138a9a8525333
UserOfNames <lightninguy.yoyo@gmail.com>
2024-07-08 14:55:09 -0400

    small improvements

--- hosts/wyvern/default.nix
+++ hosts/wyvern/default.nix
@@ -25,6 +25,12 @@
     powerManagement.enable = false;
     open = false;
     nvidiaSettings = true;
+
+    prime = {
+      # sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
 
--- scripts/tmux-sessionizer.nix
+++ scripts/tmux-sessionizer.nix
@@ -10,7 +10,7 @@ let
     if [[ $# -eq 1 ]]; then
         selected=$1
     else
-        selected=$(find ~/Projects ~/.nixosdots ~/Documents/lecture-notes -mindepth 0 -maxdepth 1 -type d | ${pkgs.fzf}/bin/fzf)
+        selected=$(find ~ ~/Projects  ~/Documents/lecture-notes -mindepth 1 -maxdepth 1 -type d | ${pkgs.fzf}/bin/fzf)
     fi
 
     if [[ -z $selected ]]; then
