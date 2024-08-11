594efe9fd800fb2277dd956e22b73b0efc47371b
UserOfNames <lightninguy.yoyo@gmail.com>
2024-08-11 14:29:24 -0400

    nixvim: added lsp keymaps

--- home/utilities/neovim/plugins/lsp.nix
+++ home/utilities/neovim/plugins/lsp.nix
@@ -4,9 +4,29 @@
   programs.nixvim.plugins.lsp = {
     enable = true;
 
-    keymaps.extra = with myLib.nixvim; [
-      (mkMap "n" "gr" "<cmd>Telescope lsp_references<Enter>" null)
-    ];
+    keymaps = {
+      diagnostic = {
+        "[d" = "goto_prev";
+        "]d" = "goto_next";
+      };
+
+      lspBuf = {
+        "K" = "hover";
+        "gd" = "definition";
+        "gD" = "declaration";
+        "go" = "type_definition";
+        "gs" = "signature_help";
+        "<F2>" = "rename";
+        "<F3>" = "format";
+        "<F4>" = "code_action";
+      };
+
+      extra = with myLib.nixvim; [
+        (mkMap "n" "gi" "<cmd>Telescope lsp_implementations<Enter>" null)
+        (mkMap "n" "gr" "<cmd>Telescope lsp_references<Enter>" null)
+      ];
+    };
+
 
     servers = {
       lua-ls.enable = true;
