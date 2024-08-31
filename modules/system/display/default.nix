9901c7f310a823eb34243370de36b47723fa76a0
UserOfNames <lightninguy.yoyo@gmail.com>
2024-08-31 15:23:39 -0400

    removed old commented lines

--- modules/system/display/default.nix
+++ modules/system/display/default.nix
@@ -4,10 +4,6 @@ let
   cfg = config.myModules.system.display;
 in {
   imports = [] ++ (myLib.importHelper ./.);
-  #   ./hyprland.nix
-  #   ./plasma.nix
-  #   ./sddm.nix
-  # ];
 
   config = lib.mkIf cfg.enable {
     programs.xwayland.enable = true;
