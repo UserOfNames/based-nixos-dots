db92085b45fcb52715954ae857c219c443448012
UserOfNames <lightninguy.yoyo@gmail.com>
2025-04-07 10:52:17 -0400

    neovim: Improved virtual text/line binds

--- home/utilities/neovim/plugins/lsp.nix
+++ home/utilities/neovim/plugins/lsp.nix
@@ -1,51 +1,69 @@
 { myLib, ... }:
 
-{
-  programs.nixvim.plugins.lsp = {
-    enable = true;
+let
+  toggle_virtual_text = ''
+    vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
+    vim.diagnostic.config({ virtual_lines = false })
+  '';
 
-    keymaps = {
-      diagnostic = {
-        "[d" = "goto_prev";
-        "]d" = "goto_next";
+  toggle_virtual_line = ''
+    vim.diagnostic.config({ virtual_lines = not vim.diagnostic.config().virtual_lines })
+    vim.diagnostic.config({ virtual_text = false })
+  '';
+in {
+  programs.nixvim = {
+    plugins.lsp = {
+      enable = true;
+
+      keymaps = {
+        diagnostic = {
+          "[d" = "goto_prev";
+          "]d" = "goto_next";
+        };
+
+        lspBuf = {
+          "K" = "hover";
+          "gd" = "definition";
+          "gD" = "declaration";
+          "go" = "type_definition";
+          "gs" = "signature_help";
+          "<F2>" = "rename";
+          "<F3>" = "format";
+          "<F4>" = "code_action";
+        };
+
+        extra = with myLib.nixvim; [
+          (mkMap "n" "gi" "<cmd>Telescope lsp_implementations<Enter>" null)
+          (mkMap "n" "gr" "<cmd>Telescope lsp_references<Enter>" null)
+          (mkFunctionMap "n" "<Leader>lh" "vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())" "Toggle inlay hints")
+          (mkFunctionMap "n" "<Leader>ld" "${toggle_virtual_text}" "Toggle inline LSP diagnostics")
+          (mkFunctionMap "n" "<Leader>ll" "${toggle_virtual_line}" "Toggle inline LSP diagnostics")
+        ];
       };
 
-      lspBuf = {
-        "K" = "hover";
-        "gd" = "definition";
-        "gD" = "declaration";
-        "go" = "type_definition";
-        "gs" = "signature_help";
-        "<F2>" = "rename";
-        "<F3>" = "format";
-        "<F4>" = "code_action";
-      };
 
-      extra = with myLib.nixvim; [
-        (mkMap "n" "gi" "<cmd>Telescope lsp_implementations<Enter>" null)
-        (mkMap "n" "gr" "<cmd>Telescope lsp_references<Enter>" null)
-        (mkFunctionMap "n" "<Leader>lh" "vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())" "Toggle inlay hints")
-      ];
+      servers = {
+        clangd.enable = true;
+
+        hls = {
+          enable = true;
+          installGhc = false;
+        };
+
+        lua_ls.enable = true;
+        nil_ls.enable = true;
+        pyright.enable = true;
+
+        rust_analyzer = {
+          enable = true;
+          installCargo = false;
+          installRustc = false;
+        };
+      };
     };
 
-
-    servers = {
-      clangd.enable = true;
-
-      hls = {
-        enable = true;
-        installGhc = false;
-      };
-
-      lua_ls.enable = true;
-      nil_ls.enable = true;
-      pyright.enable = true;
-
-      rust_analyzer = {
-        enable = true;
-        installCargo = false;
-        installRustc = false;
-      };
+    diagnostics = {
+      virtual_text = true;
     };
   };
 }
