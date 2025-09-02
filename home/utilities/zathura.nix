8b8c3fdf2814a612e628e164236a29aa81dc9712
UserOfNames <lightninguy.yoyo@gmail.com>
2025-09-02 14:24:30 -0400

    Styled zathura

--- home/utilities/zathura.nix
+++ home/utilities/zathura.nix
@@ -1,12 +1,32 @@
-{ config, lib, ... }:
+{ config, lib, myLib, ... }:
 
 let
   cfg = config.myHomeModules.utilities.zathura;
 in {
   config = lib.mkIf cfg.enable {
-    programs.zathura = {
+    programs.zathura = with myLib.base16Scheme; {
       enable = true;
       options = {
+        completion-bg	= "#${base01}";
+        completion-fg	= "#${base0D}";
+        completion-highlight-bg	= "#${base0D}";
+        completion-highlight-fg	= "#${base07}";
+        default-bg = "#${base00}";
+        default-fg = "#${base01}";
+        highlight-active-color = "#${base0D}80";
+        highlight-color	= "#${base0A}";
+        inputbar-bg	= "#${base00}";
+        inputbar-fg	= "#${base07}";
+        notification-bg	= "#${base00}";
+        notification-error-bg	= "#${base00}";
+        notification-error-fg	= "#${base08}";
+        notification-fg	= "#${base07}";
+        notification-warning-bg	= "#${base00}";
+        notification-warning-fg	= "#${base08}";
+        recolor-darkcolor	= "#${base06}";
+        recolor-lightcolor = "#${base00}";
+        statusbar-bg = "#${base02}";
+        statusbar-fg = "#${base0A}";
       };
     };
 
