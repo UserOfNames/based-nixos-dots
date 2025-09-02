c0ed121da013284e239a0aa6eb3adee9a1868c85
UserOfNames <lightninguy.yoyo@gmail.com>
2025-09-02 13:50:57 -0400

    Added central colorscheme

--- myLib/default.nix
+++ myLib/default.nix
@@ -116,4 +116,25 @@ in rec {
     mkFunctionMap = mode: key: body: desc:
       (nixvim.mkRawMap mode key ''function() ${body} end'' desc);
   };
+
+  # System base16 scheme, modified slightly from
+  # base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-city-dark.yaml";
+  base16Scheme = {
+    base00 = "1a1b26"; # Modified, original 171D23
+    base01 = "1d252c";
+    base02 = "28323a";
+    base03 = "526270";
+    base04 = "b7c5d3";
+    base05 = "d8e2ec";
+    base06 = "f6f6f8";
+    base07 = "fbfbfd";
+    base08 = "f7768e";
+    base09 = "ff9e64";
+    base0A = "b7c5d3";
+    base0B = "9ece6a";
+    base0C = "73daca"; # Modified, original 89DDFF
+    base0D = "7aa2f7";
+    base0E = "bb9af7";
+    base0F = "bb9af7";
+  };
 }
