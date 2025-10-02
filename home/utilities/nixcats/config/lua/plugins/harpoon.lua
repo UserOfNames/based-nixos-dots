42ba3e328c15cc7d8676e71e33a1d3e07d97a3ca
UserOfNames <lightninguy.yoyo@gmail.com>
2025-10-02 11:40:11 -0400

    neovim: Fixed harpoon

--- home/utilities/nixcats/config/lua/plugins/harpoon.lua
+++ home/utilities/nixcats/config/lua/plugins/harpoon.lua
@@ -10,11 +10,11 @@ return {
       hp:setup({})
 
       mk_map('n', '<Leader>hh', function() hp.ui:toggle_quick_menu(hp:list()) end, 'Open harpoon menu')
-      mk_map('n', '<Leader>ha', hp:list().add, 'Add file to harpoon')
+      mk_map('n', '<Leader>ha', function() hp:list():add() end, 'Add file to harpoon')
 
       for i = 1, 9, 1 do
          local s = tostring(i)
-         mk_map('n', '<Leader>'..s, function() hp:list():select(i) end, 'Navigate to harpoon item'..s)
+         mk_map('n', '<Leader>'..s, function() hp:list():select(i) end, 'Navigate to harpoon item '..s)
       end
    end,
 }
