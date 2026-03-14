f3ee5cd7364467f784225bcc4f3591da040b6072
UserOfNames <lightninguy.yoyo@gmail.com>
2026-03-14 01:41:25 -0400

    Fixed an incorrect gamescope setting

--- modules/other/gaming.nix
+++ modules/other/gaming.nix
@@ -4,13 +4,17 @@ let
   cfg = config.myModules.other.gaming;
 in {
   config = lib.mkIf cfg.enable {
-    programs.steam = {
-      enable = true;
-      gamescopeSession.enable = true;
+    programs = {
+      steam = {
+        enable = true;
 
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+      };
+
+      # NOTE: I haven't been using this, so I'm keeping it off for now.
+      # gamescope.enable = true;
     };
   };
 }
