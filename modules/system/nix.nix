5a4bc9829fb43fc4c950e200fb8135efcf1e98b3
UserOfNames <lightninguy.yoyo@gmail.com>
2026-06-28 12:37:13 -0400

    Updated + combined `nix` and `store` modules

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -7,10 +7,26 @@ in {
     nixpkgs.config.allowUnfree = true;
     nix = {
       nixPath = [ "nixpkgs = ${inputs.nixpkgs}" ];
-      settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+
+      optimise = {
+        automatic = true;
+        persistent = true;
+        dates = "weekly";
+      };
+
+      gc = {
+        automatic = true;
+        persistent = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+      };
     };
   };
 }
